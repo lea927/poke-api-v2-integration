e837c5806182a2cf590d89776d9079999a73d44d
Lea Tinoso <lxthym@icloud.com>
2023-10-03 00:01:33 +0800

    Creates seed, pokemons controller & views, updates routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+pokemons = [
+  {
+    name: "Pikachu",
+    base_experience: 112,
+    height: 4,
+    is_default: true,
+    order: 1,
+    weight: 60,
+    location_area_encounters: "forest"
+  },
+  {
+    name: "Charizard",
+    base_experience: 240,
+    height: 17,
+    is_default: true,
+    order: 2,
+    weight: 905,
+    location_area_encounters: "mountains"
+  }
+]
+
+pokemons.each do |pokemon_attrs|
+  Pokemon.find_or_create_by(name: pokemon_attrs[:name]) do |pokemon|
+    pokemon.base_experience = pokemon_attrs[:base_experience]
+    pokemon.height = pokemon_attrs[:height]
+    pokemon.is_default = pokemon_attrs[:is_default]
+    pokemon.order = pokemon_attrs[:order]
+    pokemon.weight = pokemon_attrs[:weight]
+    pokemon.location_area_encounters = pokemon_attrs[:location_area_encounters]
+  end
+end
