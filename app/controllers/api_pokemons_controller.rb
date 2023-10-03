ffe5ba82dd85803f36994ec0cfb14be077d29d4e
Lea Tinoso <lxthym@icloud.com>
2023-10-04 00:24:05 +0800

    Adds index and show feature for api pokemons resource to display response from api

--- app/controllers/api_pokemons_controller.rb
+++ app/controllers/api_pokemons_controller.rb
@@ -1,7 +1,11 @@
 class ApiPokemonsController < ApplicationController
+  include ApiPokemonsHelper
   def index
+    @pokemons = parsed_response_from(get_pokemons)&.dig('results')
   end
 
   def show
+    @pokemon = parsed_response_from(get_pokemon(params[:id]))
+    @image_url = @pokemon&.dig('sprites', 'front_default')
   end
 end
--- app/helpers/api_pokemons_helper.rb
+++ app/helpers/api_pokemons_helper.rb
@@ -1,2 +1,24 @@
 module ApiPokemonsHelper
+
+  BASE_URI = 'https://pokeapi.co/api/v2/pokemon/'
+
+  def get_pokemons
+    begin
+      HTTParty.get(BASE_URI)
+    rescue HTTParty::Error => e
+      Rails.logger.error(e)
+    end
+  end
+
+  def get_pokemon(name)
+    begin
+      HTTParty.get("#{BASE_URI}#{name}")
+    rescue HTTParty::Error => e
+      Rails.logger.error(e)
+    end
+  end
+
+  def parsed_response_from(response)
+    JSON.parse(response.body) if response&.body
+  end
 end
