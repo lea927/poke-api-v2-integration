163dedac9a80c009bf7540b24cb8cb57bbeec3e8
Lea Tinoso <lxthym@icloud.com>
2023-10-03 22:24:22 +0800

    Generates api pokemons controller & install httparty library to prepare index & show action

---
+++ app/controllers/api_pokemons_controller.rb
@@ -0,0 +1,7 @@
+class ApiPokemonsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :api_pokemons, only: [:index, :show]
   resources :pokemons
 
   root to: "pokemons#index"
