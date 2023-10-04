8995354df6fe2e1c2273b19e9cd1af634ccd4ea3
Lea Tinoso <lxthym@icloud.com>
2023-10-04 12:50:41 +0800

    Adds fragment caching in active record resources & low level caching for api calls to improve speed

--- app/helpers/api_pokemons_helper.rb
+++ app/helpers/api_pokemons_helper.rb
@@ -4,7 +4,9 @@ module ApiPokemonsHelper
 
   def get_pokemons
     begin
-      HTTParty.get("#{BASE_URI}?limit=100")
+      Rails.cache.fetch("get_pokemon_data", expires_in: 1.hour) do
+        HTTParty.get("#{BASE_URI}?limit=100")
+      end
     rescue HTTParty::Error => e
       Rails.logger.error(e)
     end
@@ -12,7 +14,9 @@ module ApiPokemonsHelper
 
   def get_pokemon(name)
     begin
-      HTTParty.get("#{BASE_URI}#{name}")
+      Rails.cache.fetch("get_pokemon_data", expires_in: 1.hour) do
+        HTTParty.get("#{BASE_URI}#{name}")
+      end
     rescue HTTParty::Error => e
       Rails.logger.error(e)
     end
